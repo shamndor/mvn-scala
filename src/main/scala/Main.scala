5a054c9f1b3794bff95fbd55e7a7e961626ae3f7
Ahmed M. Abdelalim <aabdelalim@thiqah.sa>
2020-05-25 21:52:27 +0300

    change 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,14 @@
-import  com.abdelalim.scala
+ 
 
 object Main{
 
-def wordCount(str:string):Int{
+def wordCount(str:String):Int = {
     str match{
         case emp if isEmpty(emp) => 0
         case str=> str.split("\\W+").length
     }
 }
 
-def isEmpty(str:string)=> str ==null || str.trim.isEmpty
+def isEmpty(str:String)=> str ==null || str.trim.isEmpty
 
 }
