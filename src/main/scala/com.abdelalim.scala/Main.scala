113bdb73cca00c0520de9d9fae4278e4d38dbc37
Ahmed M. Abdelalim <aabdelalim@thiqah.sa>
2020-05-25 21:33:24 +0300

    9 commit

--- src/main/scala/com.abdelalim.scala/Main.scala
+++ src/main/scala/com.abdelalim.scala/Main.scala
@@ -2,7 +2,7 @@ import  com.abdelalim.scala
 
 object Main{
 
-def wordCount(str:string):Int={
+def wordCount(str:string):Int{
     str match{
         case emp if isEmpty(emp) => 0
         case str=> str.split("\\W+").length
