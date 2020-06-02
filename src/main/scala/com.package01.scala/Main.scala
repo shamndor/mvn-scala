3626cd552c3ec46d38a7eed2eab7bc4bfc37cf40
ahmed <ahmed.mahmod85@hotmail.com>
2020-06-02 22:05:38 +0300

    bom1

--- src/main/scala/com.package01.scala/Main.scala
+++ src/main/scala/com.package01.scala/Main.scala
@@ -1,6 +1,12 @@
  package com.package01.scala  
 object Main{
 
+/*
+*this count funtion
+*@params str: string represent the input string we are going to count its word
+*@return integer represent number of word in the input string
+
+*/
 def wordCount(str:String):Int = {
     str match{
         case emp if isEmpty(emp) => 0      
