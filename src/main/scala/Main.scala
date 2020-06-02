7b3b56b4b222227171e9a63e108e88e14274ed9d
ahmed <ahmed.mahmod85@hotmail.com>
2020-06-02 11:37:24 +0300

    goat5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,8 @@ object Main{
 
 def wordCount(str:String):Int = {
     str match{
-        case emp if isEmpty(emp) => 0
-        case emp if emp=="-1" => -1
-        case str=> str.split("\\W+").length
+        case emp if isEmpty(emp) => 0      
+        case input=> input.split("\\W+").length
     }
      
 }
